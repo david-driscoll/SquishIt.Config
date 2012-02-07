f216fdf2a92bab1a3cce2b82220dd975c935e9a0
David Driscoll <david.driscoll@gmail.com>
2012-02-07 09:26:59 -0700

    Forgot to update the WebActivator, to have the newest version. This version also now binds to the BeginRequest EventHandler when the Application is loaded, and that will handle refreshing script caches when the application is in debug mode.

--- SquishIt.Config/App_Start/SquishitConfig.cs.pp
+++ SquishIt.Config/App_Start/SquishitConfig.cs.pp
@@ -1,10 +1,41 @@
 using System;
 using SquishIt.Config;
-[assembly: WebActivator.PostApplicationStartMethod(typeof($rootnamespace$.App_Start.SquishItConfigStartup), "PostStart")]
-namespace $rootnamespace$.App_Start {
-    public static class SquishItConfigStartup {
-        public static void PostStart() {
-            SquishIt.Config.Startup.Load();
+using System.Web;
+using TestMvcApplication;
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.SquishItConfigStartup), "PreStart")]
+namespace $rootnamespace$.App_Start
+{
+    public static class SquishItConfigStartup
+    {
+        public static void PreStart()
+        {
+            SquishIt.Config.Startup.Load(new SquishItConfigSettings() { CacheMode = SquishItCache.NamedDynamic });
+            // Register our module
+            Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(StartupBeginRequest));
 		}
+
+        public static void BeginRequest(object obj, EventArgs args)
+        {
+            if (HttpContext.Current.IsDebuggingEnabled)
+                SquishIt.Config.Startup.StaticStartup().Init();
+        }
+
+        public class StartupBeginRequest : IHttpModule
+        {
+            public void Init(HttpApplication application)
+            {
+                application.BeginRequest += new EventHandler(OnBeginRequest);
+            }
+
+            void OnBeginRequest(object sender, EventArgs e)
+            {
+                if (HttpContext.Current.IsDebuggingEnabled)
+                    SquishIt.Config.Startup.StaticStartup().Init();
+            }
+
+            public void Dispose()
+            {
+            }
+        }
     }
 }
